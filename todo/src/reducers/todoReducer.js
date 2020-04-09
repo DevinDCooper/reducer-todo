6b9204b12d90196cb9b8b565686dc585f3bd4b61
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-04-08 20:48:05 -0700

    still working

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,31 @@
+export const initialTodoState = {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      
+}
+
+export const todoReducer =(state, action) => {
+switch (action.type) {
+    case "TOGGLE_COMPLETED":
+        return {
+            ...state,
+            completed: !state.completed,
+            
+        };
+        case "UPDATE_TODO":
+        return {
+            ...state,
+            item: action.payload,
+            completed: false,
+            id: Date.now()
+
+        };
+
+        
+
+        default:
+        return state;
+}
+
+}
